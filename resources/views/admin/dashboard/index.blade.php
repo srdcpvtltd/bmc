a870b6a82da49ce6d08a14e53acd3c349f7d5cb0
srdcpvtltd <debendraster@gmail.com>
2023-11-15 11:01:59 +0530

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -179,7 +179,10 @@
             <div class="row">
                 
                 <div class="col-md-6" style="margin-left:40px;">
-                <h7 class="font-weight-semibold mb-0">Billed:</h7><span class="opacity-75">{{App\Models\Payment::where('type','monthly')->where('is_paid',0)->sum('amount')}}</span>
+                <div class="media-body">
+                <h7 class="font-weight-semibold mb-0">Billed:</h7>
+                <span class="opacity-75">{{App\Models\Payment::where('type','monthly')->where('is_paid',0)->sum('amount')}}</span>
+                <div/>
                 <h7 class="font-weight-semibold mb-0">Paid:</h7>
                 <h7 class="font-weight-semibold mb-0">Pending:</h7>
                 </div>
