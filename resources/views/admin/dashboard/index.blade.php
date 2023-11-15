d435e21843381e290e045db0ca03b0341c26ec12
srdcpvtltd <debendraster@gmail.com>
2023-11-15 11:07:23 +0530

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -176,12 +176,14 @@
             <div class="text-center">
                 <canvas id="period-billing-chart" ></canvas>
             </div>
-            <div class="row">
-                
-                <div class="col-md-6" style="margin-left:40px;">
-               <div> <h7 class="font-weight-semibold mb-0">Billed:</h7><span class="opacity-75">{{App\Models\Payment::where('type','monthly')->where('is_paid',0)->sum('amount')}}</span></div>
+            <div class="row">              
+                <div class="col-md-2" style="margin-left:40px;">
+                <h7 class="font-weight-semibold mb-0">Billed:</h7><span class="opacity-75">{{App\Models\Payment::where('type','monthly')->where('is_paid',0)->sum('amount')}}</span>
                 <h7 class="font-weight-semibold mb-0">Paid:</h7>
                 <h7 class="font-weight-semibold mb-0">Pending:</h7>
+                </div>
+                <div class="col-md-2" style="margin-left:40px;">
+                <h7 class="font-weight-semibold mb-0">Paid:</h7>
                 </div>
             </div>
             <div class="row">
