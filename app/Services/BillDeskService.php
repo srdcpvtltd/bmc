22e7f6464fcf9f6e6a72ae5bc6214b0c4aaa4119
Aounhassan275 <aounhassan275@gmail.com>
2023-11-17 00:33:44 +0500

    fix

--- app/Services/BillDeskService.php
+++ app/Services/BillDeskService.php
@@ -35,7 +35,6 @@ class BillDeskService
                     "user_agent" => request()->header('User-Agent'),
                 ]
             ];
-            dd($payload);
             $header = base64_encode(json_encode($headers));
             $payload = base64_encode(json_encode($payload));
             $signature = hash_hmac('sha256', "$header.$payload", 'Kr7mREYKcU9E0HExLpb1grnxVqsf9YfI', true);
