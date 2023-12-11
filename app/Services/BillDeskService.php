3ce424654ad740f8bf39a4e7bd66d38abb3ab2d9
Aounhassan275 <aounhassan275@gmail.com>
2023-12-11 16:01:05 +0500

    Testing

--- app/Services/BillDeskService.php
+++ app/Services/BillDeskService.php
@@ -53,6 +53,7 @@ class BillDeskService
                 "BD-Traceid:".$tracid,
                 "BD-Timestamp: 20200817132207"
             );
+            dd($ch_headers);
             curl_setopt( $ch, CURLOPT_HTTPHEADER, $ch_headers);
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt( $ch, CURLOPT_POSTFIELDS, $curl_payload);
