105089a6a84160548ff2de7d16644536784c8748
Aounhassan275 <aounhassan275@gmail.com>
2024-01-26 23:33:03 +0500

    test

--- app/Http/Controllers/CollectionStaff/PaymentController.php
+++ app/Http/Controllers/CollectionStaff/PaymentController.php
@@ -59,11 +59,13 @@ class PaymentController extends Controller
             if($payment->is_paid)
             {
                 $phone = $payment->phone ? $payment->phone : @$payment->shop->phone;
+                dd($phone);
                 if($phone && strlen($phone) == 10)
                 {
                     (new SmsService())->sendSMS($phone);
                 }
             }
+            dd(2);
             if($payment->payment_mode == "UPI")
             {
                 return redirect()->to(route('collection_staff.payment.show',$payment->id));
